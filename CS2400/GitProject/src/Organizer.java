42ca72f3ef50b8f0cc4fd1c40a7f7f7393eb14a8
Ilan Ascencio <ilan.sobj.07@gmail.com>
2026-01-29 03:29:58 -0800

    Feature complete

--- CS2400/GitProject/src/Organizer.java
+++ CS2400/GitProject/src/Organizer.java
@@ -13,7 +13,7 @@ public class Organizer {
 
 		ArrayList <User> userDatabase = createUserDatabase();
 
-		userDatabase = organizeByID(userDatabase);
+		userDatabase = organizeByFirst(userDatabase);
 
 		for( User user : userDatabase){
 			System.out.println(user);
@@ -22,6 +22,21 @@ public class Organizer {
 	}
 
 	public static ArrayList <User> organizeByFirst(ArrayList <User> database){
+
+		
+		for(int i = 0 ; i < database.size() - 1 ; i++){
+
+			for(int j = 0 ; j < database.size() - 1 - i ; j++){
+				if( database.get(j).first_name().compareTo(database.get( j + 1 ).first_name()) < -1 ){
+				User temp = database.get(j);
+				database.set(j, database.get(j + 1));
+				database.set(j + 1, temp);
+				}
+			}
+			
+		}
+
+		return database;
 		
 	}
 
